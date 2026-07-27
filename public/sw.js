853daac7fe50b06568de010eafca8357a248e4fd
wahaha-art <xcyzxcv@163.com>
2026-07-27 16:42:14 +0800

    优化饮品图片加载和手机端选择框

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,16 @@
-const CACHE = 'xiaoyang-workbench-v2'
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg']
+const CACHE = 'xiaoyang-workbench-v3'
+const BRAND_LOGOS = [
+  './brands/luckin-cropped.webp',
+  './brands/mollytea.webp',
+  './brands/alittletea.webp',
+  './brands/heytea.webp',
+  './brands/chayan-icon.webp',
+  './brands/manner.webp',
+  './brands/guming-icon.webp',
+  './brands/chabaidao-icon.webp',
+  './brands/chagee.webp',
+]
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', ...BRAND_LOGOS]
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)))
@@ -15,11 +26,23 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return
+  const requestUrl = new URL(event.request.url)
+  const isLocalImage = requestUrl.origin === self.location.origin && event.request.destination === 'image'
+
+  if (isLocalImage) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
+        if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()))
+        return response
+      }))
+    )
+    return
+  }
+
   event.respondWith(
     fetch(event.request)
       .then((response) => {
-        const copy = response.clone()
-        caches.open(CACHE).then((cache) => cache.put(event.request, copy))
+        if (response.ok) caches.open(CACHE).then((cache) => cache.put(event.request, response.clone()))
         return response
       })
       .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
